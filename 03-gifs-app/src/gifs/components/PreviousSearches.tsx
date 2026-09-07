eeba00d412671d17d8cd23ac247854b6ea798921
Carlos Espino Maraví <espinomaravic@gmail.com>
2026-09-07 18:24:37 -0500

    Section 6: add initial features (style. font and components).

---
+++ 03-gifs-app/src/gifs/components/PreviousSearches.tsx
@@ -0,0 +1,21 @@
+import type { FC } from "react";
+
+interface Props {
+  searches: string[];
+}
+
+export const PreviousSearches: FC<Props> = ({ searches = [] }) => {
+  return (
+    <div className="previous-searches">
+      <h2>Búsquedas previas</h2>
+      <ul className="previous-searches-list">
+        {searches.map((term) => (
+          <li key={term}>{term}</li>
+        ))}
+        {/* <li>Goku</li>
+        <li>Saitama</li>
+        <li>Elder Ring</li> */}
+      </ul>
+    </div>
+  );
+};
---
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+// rafc
+
+interface Props {
+  placeholder?: string;
+}
+
+export const SearchBar = ({ placeholder = 'Buscar' }: Props) => {
+  return (
+    <div className="search-container">
+      <input type="text" placeholder={placeholder} />
+      <button>Buscar</button>
+    </div>
+  );
+};
