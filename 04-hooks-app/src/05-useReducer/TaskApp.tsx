04d20c1ab4f2673cc1dbfa0162a00832c9b90553
Carlos Espino Maraví <espinomaravic@gmail.com>
2026-09-21 16:19:02 -0500

    Section 10: use reducer in TaskApp component, add localStorage persistence and Zod object validation.

--- 04-hooks-app/src/05-useReducer/TaskApp.tsx
+++ 04-hooks-app/src/05-useReducer/TaskApp.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
 import { Plus, Trash2, Check } from "lucide-react";
 
@@ -7,63 +7,36 @@ import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-import type { Todo } from "./reducer/taskReducer";
-
-/*
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
-*/
+import { getTasksInitialState, taskReducer } from "./reducer/taskReducer";
 
 export const TasksApp = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState("");
+
+  const [state, dispatch] = useReducer(taskReducer, getTasksInitialState());
+
+  useEffect(() => {
+    localStorage.setItem("tasks-state", JSON.stringify(state));
+  }, [state]);
 
   const addTodo = () => {
     if (inputValue.length === 0) return;
-    // console.log("Agregar tarea", inputValue);
-
-    const newTodo: Todo = {
-      id: Date.now(),
-      text: inputValue.trim(),
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
-    // setTodos((prev) => {
-    //   [...prev, newTodo];
-    // });
+    dispatch({ type: "ADD_TODO", payload: inputValue });
     setInputValue("");
   };
 
   const toggleTodo = (id: number) => {
-    console.log("Cambiar de true a false", id);
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        // todo.completed = !todo.completed;
-        return { ...todo, completed: !todo.completed };
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
+    dispatch({ type: "TOGGLE_TODO", payload: id });
   };
 
   const deleteTodo = (id: number) => {
-    // console.log("Eliminar tarea", id);
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    // console.log({ newTodos: updatedTodos });
-    setTodos(updatedTodos);
+    dispatch({ type: "DELETE_TODO", payload: id });
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
-    // console.log("Presiono enter");
-    // console.log({ key: e.key });
     if (e.key === "Enter") addTodo();
   };
 
-  const completedCount = todos.filter((todo) => todo.completed).length;
-  const totalCount = todos.length;
+  const { todos, completed: completedCount, length: totalCount } = state;
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 p-4">
--- 04-hooks-app/src/05-useReducer/reducer/taskReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/taskReducer.ts
@@ -1,3 +1,5 @@
+import * as z from "zod";
+
 export interface Todo {
   id: number;
   text: string;
@@ -15,6 +17,48 @@ export type TaskAction =
   | { type: "ADD_TODO"; payload: string }
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
+
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateSchema = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
+export const getTasksInitialState = (): TaskState => {
+  const localStorageState = localStorage.getItem("tasks-state");
+  if (!localStorageState)
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+
+  // Validar mediante Zod
+  // const result = TaskStateSchema.safeParse(localStorageState);
+  const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+
+  if (result.error) {
+    console.log(result.error);
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  // !Cuidado porque el objeto puede haber sido manipulado
+  // return JSON.parse(localStorageState);
+  return result.data;
+};
 
 export const taskReducer = (
   state: TaskState,
